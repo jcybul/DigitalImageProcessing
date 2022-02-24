695ad3dbdcff8e508a35515156224fd19d292969
TianYuFan1 <tianyufan01@gmail.com>
2022-02-24 14:29:27 -0500

    starting pytorch

--- binary_map.py
+++ binary_map.py
@@ -27,6 +27,9 @@ img2 = cv.imread('res/test/00023966/20151119_081621.jpg')
 # img1 = cv.imread('res/test/00021510/20151102_060125.jpg')
 # img2 = cv.imread('res/test/00021510/20151108_160137.jpg')
 
+img1 = cv.resize(img1, (700, 500), interpolation=cv.INTER_AREA)
+img2 = cv.resize(img2, (700, 500), interpolation=cv.INTER_AREA)
+
 img1_night = False
 img2_night = False
 
@@ -38,83 +41,38 @@ img2_hsv = cv.cvtColor(img2, cv.COLOR_RGB2HSV)
 img1_brightness = img1_hsv[..., 2].mean()
 img2_brightness = img2_hsv[..., 2].mean()
 
-if img1_brightness < 50:
-    img1_night = True
-if img2_brightness < 50:
-    img2_night = True
+scale = 0.2
+t11 = img1_brightness * scale
+t12 = img1_brightness * scale
+t21 = img2_brightness * scale
+t22 = img2_brightness * scale
 
-img1 = cv.resize(img1, (300, 200), interpolation=cv.INTER_AREA)
-img2 = cv.resize(img2, (300, 200), interpolation=cv.INTER_AREA)
+[img1, img2] = match_images(img1, img2)
 
-t11 = 100
-t12 = 150
-t21 = 100
-t22 = 150
+img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
-if img1_night:
-    t11 = 40
-    t12 = 50
-if img2_night:
-    t21 = 40
-    t22 = 50
-
+kernel = np.ones((3, 3), np.float32)/25
+img1 = cv.filter2D(img1, -1, kernel)
+img2 = cv.filter2D(img2, -1, kernel)
 
 img1 = cv.Canny(image=img1, threshold1=t11, threshold2=t12)
 img2 = cv.Canny(image=img2, threshold1=t21, threshold2=t22)
+img1 = cv.Canny(image=img1, threshold1=t11, threshold2=t12)
+img2 = cv.Canny(image=img2, threshold1=t21, threshold2=t22)
+
+kernel = np.ones((3, 3), np.uint8)
+img1 = cv.dilate(img1, kernel, iterations=1)
+img2 = cv.dilate(img2, kernel, iterations=1)
 
 img_out = np.concatenate((img1, img2), axis=1)
 cv.imshow('Images', img_out)
 cv.waitKey(0)
 
+xor = cv.bitwise_xor(img1, img2, mask=None)
+xor_not = cv.bitwise_not(xor)
 
-# b1 = img1.copy()
-# b1[:, :, 1] = 0
-# b1[:, :, 2] = 0
+print(cv.countNonZero(xor_not))
 
-# g1 = img1.copy()
-# g1[:, :, 0] = 0
-# g1[:, :, 2] = 0
-
-# r1 = img1.copy()
-# r1[:, :, 0] = 0
-# r1[:, :, 1] = 0
-
-# # Second Image
-# b2 = img2.copy()
-# b2[:, :, 1] = 0
-# b2[:, :, 2] = 0
-
-# g2 = img2.copy()
-# g2[:, :, 0] = 0
-# g2[:, :, 2] = 0
-
-# r2 = img2.copy()
-# r2[:, :, 0] = 0
-# r2[:, :, 1] = 0
-
-# t11 = 100
-# t12 = 150
-# t21 = 100
-# t22 = 150
-
-# if img1_night:
-#     t11 = 40
-#     t12 = 80
-# if img2_night:
-#     t21 = 40
-#     t22 = 80
-
-# b1 = cv.Canny(image=b1, threshold1=t11, threshold2=t12)
-# g1 = cv.Canny(image=g1, threshold1=t11, threshold2=t12)
-# r1 = cv.Canny(image=r1, threshold1=t11, threshold2=t12)
-
-# b2 = cv.Canny(image=b2, threshold1=t21, threshold2=t22)
-# g2 = cv.Canny(image=g2, threshold1=t21, threshold2=t22)
-# r2 = cv.Canny(image=r2, threshold1=t21, threshold2=t22)
-
-# img_out1 = np.concatenate((b1, g1, r1), axis=1)
-# img_out2 = np.concatenate((b2, g2, r2), axis=1)
-
-# img_out = np.concatenate((img_out1, img_out2), axis=1)
-# cv.imshow('Images', img_out)
-# cv.waitKey(0)
+cv.imshow('Images', xor_not)
+cv.waitKey(0)
